8fbe522c1867bba04b09eca68ba4d89a90794be1
GabrielCrackPro <gabrielcrackpro2001@gmail.com>
2022-12-25 12:43:04 +0100

    fix useScreen hook 🔧

--- src/hooks/useScreenSize.js
+++ src/hooks/useScreenSize.js
@@ -4,14 +4,12 @@ import { useState, useEffect } from "react";
 
 const useScreenSize = () => {
   
-  // TODO: Fix screen sizes
-
   const [isPhoneScreen, setIsPhoneScreen] = useState(false);
   const [isTabletScreen, setIsTabletScreen] = useState(false);
   const [isDesktopScreen, setIsDesktopScreen] = useState(false);
   const [isLargeDesktopScreen, setIsLargeDesktopScreen] = useState(false);
 
-  const phoneScreenQuery = useMediaQuery("(min-width:481px) and (max-width:768px)");
+  const phoneScreenQuery = useMediaQuery("(max-width:768px)");
   const tabletScreenQuery = useMediaQuery("(min-width:769px) and (max-width:1024px)");
   const desktopScreenQuery = useMediaQuery("(min-width:1025px) and (max-width:1440px)");
   const largeDesktopScreenQuery = useMediaQuery("(min-width:1441px)");
