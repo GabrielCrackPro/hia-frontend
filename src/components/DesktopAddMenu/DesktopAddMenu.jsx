6c112fc039825553c019a442b73f663eb3cdcacc
GabrielCrackPro <gabrielcrackpro2001@gmail.com>
2022-12-26 15:05:37 +0100

    Icons added to the desktop menu 💻

--- src/components/DesktopAddMenu/DesktopAddMenu.jsx
+++ src/components/DesktopAddMenu/DesktopAddMenu.jsx
@@ -2,6 +2,10 @@
 import React, { useState } from "react";
 import { Button, Menu, MenuItem } from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
+import InventoryIcon from "@mui/icons-material/Inventory";
+import StoreIcon from "@mui/icons-material/Store";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import CameraAltIcon from "@mui/icons-material/CameraAlt";
 
 const DesktopAddMenu = () => {
   const [anchorEl, setAnchorEl] = useState(null);
@@ -23,10 +27,10 @@ const DesktopAddMenu = () => {
     <>
       <Button id="basic-button" aria-controls={open ? "add-menu" : undefined} aria-haspopup="true" aria-expanded={open ? "true" : undefined} onClick={handleMenuOpen} sx={{ textTransform: "revert" }}><AddIcon /> Add New</Button>
       <Menu id="add-menu" anchorEl={anchorEl} open={open} onClose={handleMenuClose} MenuListProps={{ "aria-labelledby": "basic-button" }}>
-        <MenuItem onClick={() => handleMenuSelected("item")}>Item</MenuItem>
-        <MenuItem onClick={() => handleMenuSelected("shop")}>Shop</MenuItem>
-        <MenuItem onClick={() => handleMenuSelected("location")}>Location</MenuItem>
-        <MenuItem onClick={() => handleMenuSelected("scan")}>Scan Item</MenuItem>
+        <MenuItem onClick={() => handleMenuSelected("item")}><InventoryIcon sx={{ fontSize: "16px", padding: "2px" }} /> Item</MenuItem>
+        <MenuItem onClick={() => handleMenuSelected("shop")}><StoreIcon sx={{ fontSize: "16px", padding: "2px" }} />  Shop</MenuItem>
+        <MenuItem onClick={() => handleMenuSelected("location")}><LocationOnIcon sx={{ fontSize: "16px", padding: "2px" }} /> Location</MenuItem>
+        <MenuItem onClick={() => handleMenuSelected("scan")}><CameraAltIcon sx={{ fontSize: "16px", padding: "2px" }} /> Scan Item</MenuItem>
       </Menu>
     </>
   );
