1197630c0f62e93a680f5fdabb8c4b34681034ec
GabrielCrackPro <gabrielcrackpro2001@gmail.com>
2022-12-27 14:20:47 +0100

    make items responsive 💻

--- src/components/Routes/Items/Items.jsx
+++ src/components/Routes/Items/Items.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
 import React, { useState, useEffect } from "react";
-import { Card, Link, Typography, CardContent, capitalize } from "@mui/material";
+import { Card, Link, Typography, CardContent, capitalize, Grid } from "@mui/material";
 import InventoryIcon from "@mui/icons-material/Inventory";
 import NotesIcon from "@mui/icons-material/Notes";
 import CalendarTodayIcon from "@mui/icons-material/CalendarToday";
@@ -13,61 +13,65 @@ import Navbar from "../../Navbar/Navbar";
 import { putData } from "../../../utils";
 import { Box } from "@mui/system";
 import PlusButton from "../../PlusButton/PlusButton";
-
+import useScreenSize from "../../../hooks/useScreenSize";
 const Items = () => {
   const [data, setData] = useState([]);
   const loggedUser = JSON.parse(localStorage.getItem("USER"));
+
+  const { isPhoneScreen, isTabletScreen } = useScreenSize();
+
   useEffect(() => {
     (async () => {
       setData(loggedUser.items);
     })();
   }, []);
-
   return (
     <>
       <Titlebar />
       <Navbar value="items" />
-      <PlusButton />
-      {
-        data.length ?
-          data.map((entry) => {
-            return (
-              <Card variant="outlined" key={entry["created_at"]}>
-                <CardContent>
-                  <Typography variant="p"><InventoryIcon /> {"Name: " + capitalize(entry.name)}</Typography> <br />
-                  <Typography variant="p"><NotesIcon /> {"Description: " + capitalize(entry.description)}</Typography> <br />
-                  <Typography variant="p"><CategoryIcon /> {"Type: " + capitalize(entry.type)}</Typography> <br />
-                  <Typography variant="p"><CalendarTodayIcon /> {"Created At : " + new Date(entry.created_at).toLocaleString()}</Typography> <br />
-                  <Typography variant="p"><EventRepeatIcon /> {"Updated At: " + new Date(entry.updated_at).toLocaleString()}</Typography> <br />
-                  <Box component="div" sx={{ mt: "5px" }}>
-                    <Link href={`item/${entry["_id"]}`} sx={{ mr: "10px", border: "1px solid currentcolor", padding: "3px" }} underline="none" component="a" variant="button"><EditIcon sx={{ fontSize: "15px", ml: "5px" }} />Edit Item</Link>
-                    <Link onClick={() => {
-                      loggedUser.items.pop();
-                      localStorage.setItem("USER", JSON.stringify(loggedUser));
-                      putData(`http://127.0.0.1:3001/api/v1/users/${loggedUser["_id"]}`, loggedUser);
-                      // TODO: Remove a specific element
-                      location.pathname = "/home";
-                    }} sx={{ border: "1px solid currentcolor", padding: "3px" }} underline="none" component="a" variant="button" color="error"><DeleteIcon sx={{ fontSize: "15px", ml: "5px" }} />Delete Item</Link>
-                  </Box>
-                </CardContent>
-              </Card>
-            );
-          })
-          :
-          <Box sx={{
-            width: "100vw",
-            height: "87vh",
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            justifyContent: "center",
-            color: "#b8b8b8"
-          }}>
-            <InventoryIcon sx={{ fontSize: "75px" }} />
-            <Typography sx={{ fontSize: "35px", fontWeight: "bold" }}>No items yet</Typography>
-          </Box>
-
-      }
+      {isPhoneScreen || isTabletScreen ? <PlusButton /> : ""}
+      <Navbar value="items" />
+      <Typography variant="h3" textAlign="center" marginTop="5px">Items</Typography>
+      <Grid alignItems="center" container={!isPhoneScreen} spacing={2} sx={{ marginTop: isPhoneScreen ? "0px" : "20px" }}>
+        {
+          data.length ?
+            data.map((entry) => {
+              return (
+                <Card variant="outlined" key={entry["created_at"]}>
+                  <CardContent>
+                    <Typography variant="p"><InventoryIcon /> {"Name: " + capitalize(entry.name)}</Typography> <br />
+                    <Typography variant="p"><CategoryIcon /> {"Type: " + capitalize(entry.type)}</Typography> <br />
+                    <Typography variant="p"><CalendarTodayIcon /> {"Created At : " + new Date(entry.created_at).toLocaleString()}</Typography> <br />
+                    <Typography variant="p"><EventRepeatIcon /> {"Updated At: " + new Date(entry.updated_at).toLocaleString()}</Typography> <br />
+                    <Box component="div" sx={{ mt: "5px" }}>
+                      <Link href={`item/${entry["_id"]}`} sx={{ mr: "10px", border: "1px solid currentcolor", padding: "3px" }} underline="none" component="a" variant="button"><EditIcon sx={{ fontSize: "15px", ml: "5px" }} />Edit Item</Link>
+                      <Link onClick={() => {
+                        loggedUser.items.pop();
+                        localStorage.setItem("USER", JSON.stringify(loggedUser));
+                        putData(`http://127.0.0.1:3001/api/v1/users/${loggedUser["_id"]}`, loggedUser);
+                        // TODO: Remove a specific element
+                        location.pathname = "/home";
+                      }} sx={{ border: "1px solid currentcolor", padding: "3px" }} underline="none" component="a" variant="button" color="error"><DeleteIcon sx={{ fontSize: "15px", ml: "5px" }} />Delete Item</Link>
+                    </Box>
+                  </CardContent>
+                </Card>
+              );
+            })
+            :
+            <Box sx={{
+              width: "100vw",
+              height: "87vh",
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              justifyContent: "center",
+              color: "#b8b8b8"
+            }}>
+              <InventoryIcon sx={{ fontSize: "75px" }} />
+              <Typography sx={{ fontSize: "35px", fontWeight: "bold" }}>No items yet</Typography>
+            </Box>
+        }
+      </Grid>
     </>
   );
 };
