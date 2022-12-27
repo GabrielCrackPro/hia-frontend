4d03cad2fe57663e7ef19771e17e615a9a5bdea5
GabrielCrackPro <gabrielcrackpro2001@gmail.com>
2022-12-27 14:32:01 +0100

    make shops responsive 💻

--- src/components/Routes/Shops/Shops.jsx
+++ src/components/Routes/Shops/Shops.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
 import React, { useState, useEffect } from "react";
-import { Card, Link, Typography, CardContent, capitalize } from "@mui/material";
+import { Card, Link, Typography, CardContent,Grid } from "@mui/material";
 import StoreIcon from "@mui/icons-material/Store";
 import NotesIcon from "@mui/icons-material/Notes";
 import CalendarTodayIcon from "@mui/icons-material/CalendarToday";
@@ -15,7 +15,9 @@ import Titlebar from "../../Titlebar/TitleBar";
 import Navbar from "../../Navbar/Navbar";
 import { putData, deleteOne } from "../../../utils";
 import { Box } from "@mui/system";
+import { capitalize } from "../../../utils";
 import PlusButton from "../../PlusButton/PlusButton";
+import useScreenSize from "../../../hooks/useScreenSize";
 
 const Shops = () => {
   const loggedUser = JSON.parse(localStorage.getItem("USER"));
@@ -26,53 +28,57 @@ const Shops = () => {
     })();
   }, []);
 
+  const { isPhoneScreen, isTabletScreen } = useScreenSize();
   return (
     <>
       <Titlebar />
       <Navbar value="shops" />
-      <PlusButton />
-      {
-        data.length ?
-          data.map((entry) => {
-            return (
-              <Card variant="outlined" key={entry["created_at"]}>
-                <CardContent>
-                  <Typography variant="p"><StoreIcon /> {"Name: " + capitalize(entry.name)}</Typography> <br />
-                  <Typography variant="p"><NotesIcon /> {"Description: " + capitalize(entry.description)}</Typography> <br />
-                  <Typography variant="p"><ShoppingCartIcon /> {"Type: " + capitalize(entry.type)}</Typography> <br />
-                  <Typography variant="p"> {entry.address.includes("www.") ? <><LanguageIcon /> <Link href={`http://${entry.address}`} target="_blank">{entry.address}</Link></> : <><BusinessIcon /> {capitalize(entry.address)}</>} </Typography> <br />
-                  <Typography variant="p"><ScheduleIcon /> {"Schedule: " + capitalize(entry.schedule)}</Typography> <br />
-                  <Typography variant="p"><CalendarTodayIcon /> {"Created At: " + new Date(entry.created_at).toDateString()}</Typography> <br />
-                  <Typography variant="p"><EventRepeatIcon /> {"Updated At: " + new Date(entry.updated_at).toDateString()}</Typography> <br />
-                  <Box component="div" sx={{ mt: "5px" }}>
-                    <Link href={`shop/${entry["_id"]}`} sx={{ mr: "10px", border: "1px solid currentcolor", padding: "3px" }} underline="none" component="a" variant="button"><EditIcon sx={{ fontSize: "15px", ml: "5px" }} />Edit Shop</Link>
-                    <Link onClick={() => {
-                      deleteOne("shops", entry["_id"]);
-                      loggedUser.shops.pop();
-                      localStorage.setItem("USER", JSON.stringify(loggedUser));
-                      putData(`http://127.0.0.1:3001/api/v1/users/${loggedUser["_id"]}`, loggedUser);
-                      // TODO: Remove a specific element
-                    }}
-                    sx={{ border: "1px solid currentcolor", padding: "3px" }} underline="none" component="a" variant="button" color="error"><DeleteIcon sx={{ fontSize: "15px", ml: "5px" }} />Delete Shop</Link>
-                  </Box>
-                </CardContent>
-              </Card>
-            );
-          })
-          :
-          <Box sx={{
-            width: "100vw",
-            height: "87vh",
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            justifyContent: "center",
-            color: "#b8b8b8"
-          }}>
-            <StoreIcon sx={{ fontSize: "75px" }} />
-            <Typography sx={{ fontSize: "35px", fontWeight: "bold" }}>No shops yet</Typography>
-          </Box>
-      }
+      {isPhoneScreen && isTabletScreen ? <PlusButton /> : ""}
+      <Typography variant="h3" textAlign="center">Shops</Typography>
+      <Grid alignItems="center" container={!isPhoneScreen} spacing={2} sx={{ marginTop: isPhoneScreen ? "0px" : "20px" }}>
+        {
+          data.length ?
+            data.map((entry) => {
+              return (
+                <Card variant="outlined" key={entry["created_at"]}>
+                  <CardContent>
+                    <Typography variant="p"><StoreIcon /> {"Name: " + capitalize(entry.name)}</Typography> <br />
+                    <Typography variant="p"><NotesIcon /> {"Description: " + capitalize(entry.description)}</Typography> <br />
+                    <Typography variant="p"><ShoppingCartIcon /> {"Type: " + capitalize(entry.type)}</Typography> <br />
+                    <Typography variant="p"> {entry.address.includes("www.") ? <><LanguageIcon /> <Link href={`http://${entry.address}`} target="_blank">{entry.address}</Link></> : <><BusinessIcon /> {capitalize(entry.address)}</>} </Typography> <br />
+                    <Typography variant="p"><ScheduleIcon /> {"Schedule: " + capitalize(entry.schedule)}</Typography> <br />
+                    <Typography variant="p"><CalendarTodayIcon /> {"Created At: " + new Date(entry.created_at).toDateString()}</Typography> <br />
+                    <Typography variant="p"><EventRepeatIcon /> {"Updated At: " + new Date(entry.updated_at).toDateString()}</Typography> <br />
+                    <Box component="div" sx={{ mt: "5px" }}>
+                      <Link href={`shop/${entry["_id"]}`} sx={{ mr: "10px", border: "1px solid currentcolor", padding: "3px" }} underline="none" component="a" variant="button"><EditIcon sx={{ fontSize: "15px", ml: "5px" }} />Edit Shop</Link>
+                      <Link onClick={() => {
+                        deleteOne("shops", entry["_id"]);
+                        loggedUser.shops.pop();
+                        localStorage.setItem("USER", JSON.stringify(loggedUser));
+                        putData(`http://127.0.0.1:3001/api/v1/users/${loggedUser["_id"]}`, loggedUser);
+                        // TODO: Remove a specific element
+                      }}
+                      sx={{ border: "1px solid currentcolor", padding: "3px" }} underline="none" component="a" variant="button" color="error"><DeleteIcon sx={{ fontSize: "15px", ml: "5px" }} />Delete Shop</Link>
+                    </Box>
+                  </CardContent>
+                </Card>
+              );
+            })
+            :
+            <Box sx={{
+              width: "100vw",
+              height: "87vh",
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              justifyContent: "center",
+              color: "#b8b8b8"
+            }}>
+              <StoreIcon sx={{ fontSize: "75px" }} />
+              <Typography sx={{ fontSize: "35px", fontWeight: "bold" }}>No shops yet</Typography>
+            </Box>
+        }
+      </Grid>
     </>
   );
 };
