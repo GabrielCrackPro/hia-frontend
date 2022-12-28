f9a6ace62e584b8216064dc52a9e0daa3dd289c7
GabrielCrackPro <gabrielcrackpro2001@gmail.com>
2022-12-28 14:11:29 +0100

    make settings responsive 💻

--- src/components/Routes/Settings/Settings.jsx
+++ src/components/Routes/Settings/Settings.jsx
@@ -8,12 +8,14 @@ import AlternateEmailIcon from "@mui/icons-material/AlternateEmail";
 import InventoryIcon from "@mui/icons-material/Inventory";
 import EditIcon from "@mui/icons-material/Edit";
 import DeleteIcon from "@mui/icons-material/Delete";
+import useScreenSize from "../../../hooks/useScreenSize";
 import { Box } from "@mui/system";
 import { Avatar, Button, Card, CardContent, CardActions } from "@mui/material";
 import { postData } from "../../../utils";
 import { SlideUp } from "../../Animation/Animation";
 const Settings = () => {
   const loggedUser = JSON.parse(localStorage.getItem("USER"));
+  const { isPhoneScreen, isTabletScreen } = useScreenSize();
   const handleLogout = () => {
     (async () => {
       const response = await postData(`http://127.0.0.1:3001/api/v1/users/logout/${loggedUser["_id"]}`, JSON.parse(localStorage.getItem("USER")));
@@ -26,10 +28,10 @@ const Settings = () => {
   return (
     <>
       <Navbar value="settings" />
-      <Titilebar />
+      {isPhoneScreen || isTabletScreen ? <Titilebar /> : ""}
       <SlideUp>
-        <Box>
-          <Card sx={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center" }}>
+        <Box sx={!isPhoneScreen || !isTabletScreen ? { marginTop: "60px" } : {}}>
+          <Card sx={!isPhoneScreen && !isTabletScreen ? { display: "flex", alignItems: "center", justifyContent: "space-around" } : { display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
             <Avatar src={loggedUser.profilePicture} sx={{ marginTop: "15px", height: "200px", width: "200px" }} />
             <CardContent>
               <Typography variant="h4" component="h4" textAlign="center">
@@ -44,10 +46,11 @@ const Settings = () => {
             </CardContent>
             <CardActions>
               <Button size="small" variant="contained" color="secondary"><EditIcon />Edit</Button>
+              {!isPhoneScreen || !isTabletScreen ? <Button size="small" variant="contained" color="secondary" onClick={handleLogout}><LogoutIcon /> Log Out</Button> : ""}
               <Button size="small" variant="contained" color="error"><DeleteIcon /> Delete</Button>
             </CardActions>
           </Card>
-          <Button variant="contained" color="secondary" fullWidth sx={{ marginTop: "10px" }} onClick={handleLogout}><LogoutIcon /> Log out</Button>
+          {!isPhoneScreen && !isTabletScreen ? "" : <Button variant="contained" color="secondary" fullWidth sx={{ marginTop: "10px" }} onClick={handleLogout}><LogoutIcon /> Log Out</Button>}
         </Box>
       </SlideUp>
     </>
