5c6263ac004ee772db9c31285c047ce35888c859
GabrielCrackPro <gabrielcrackpro2001@gmail.com>
2023-01-01 11:18:05 +0100

    animations timeout updated 🔧

--- src/components/Animation/Animation.jsx
+++ src/components/Animation/Animation.jsx
@@ -1,10 +1,12 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
-import { Alert, Slide, Zoom } from "@mui/material";
+import { Slide, Zoom } from "@mui/material";
+
+const animationTime = 200;
 
 const SlideUp = ({ children }) => {
   return (
-    <Slide direction="up" in mountOnEnter unmountOnExit timeout={500}>
+    <Slide direction="up" in mountOnEnter unmountOnExit timeout={animationTime}>
       {children}
     </Slide>
   );
@@ -12,7 +14,7 @@ const SlideUp = ({ children }) => {
 
 const ZoomIn = ({ children }) => {
   return (
-    <Zoom in mountOnEnter unmountOnExit timeout={500}>
+    <Zoom in mountOnEnter unmountOnExit timeout={animationTime}>
       {children}
     </Zoom>
   );
